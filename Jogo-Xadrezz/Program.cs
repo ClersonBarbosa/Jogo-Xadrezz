8e3d668a8ba0f52dbd5c0f101c2b1c18f0a73102
Clerson de Freitas <clersonfreitas@yahoo.com.br>
2020-03-03 17:57:41 -0300

    Criando a classe Posicao

--- Jogo-Xadrezz/Program.cs
+++ Jogo-Xadrezz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace Jogo_Xadrezz
 {
@@ -6,6 +7,9 @@ namespace Jogo_Xadrezz
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
         }
     }
 }
